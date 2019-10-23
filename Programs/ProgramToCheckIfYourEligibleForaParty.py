854369c6ace5d875e307dcad39c6d9e68c2de8be
harshmody30401 <56110469+harshmody30401@users.noreply.github.com>
2019-10-23 21:52:30 +0530

    Create ProgramToCheckIfYourEligibleForaParty.py

---
+++ Programs/ProgramToCheckIfYourEligibleForaParty.py
@@ -0,0 +1,7 @@
+name = input("Please Enter Your Name : ")
+print("Hello {0}.".format(name))
+age = int(input("How Old Are You? : "))
+if 21 < age < 31:
+    print("welcome to the party")
+else :
+    print ("Sorry you do not meet the given guidelines of age")
